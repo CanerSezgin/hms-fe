a589ec236fe3dd6f9964e9d04b383678e2665df3
Caner Sezgin <ccanersezgin@gmail.com>
2021-12-24 16:59:52 +0300

    doctor dashboard

--- src/views/Doctor/index.vue
+++ src/views/Doctor/index.vue
@@ -9,6 +9,7 @@
           />
         </v-avatar>
 
+        <div>Caner Sezgin</div>
         <div>caner@sezgin.com</div>
       </v-sheet>
 
@@ -16,20 +17,20 @@
 
       <v-list>
         <v-list-item
-          v-for="[icon, text, to] in links"
-          :key="icon"
+          v-for="link in links"
+          :key="link.icon"
           link
           exact
-          :to="`/doctor/${to}`"
+          :to="`/doctor/${link.to}`"
           active-class="primary white--text"
           class="py-3 px-2"
         >
           <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
+            <v-icon>{{ link.icon }}</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -38,6 +39,41 @@
     <v-container class="py-8 px-6" fluid>
       <v-row>
         <v-col cols="12">
+          <!-- Dashboard -->
+          <div class="mt-16" v-if="isHomepage">
+            <v-card class="mx-auto" max-width="344" outlined>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="text-h5 mb-1">
+                    Caner Sezgin
+                  </v-list-item-title>
+                  <div class="text-overline">Allergy</div>
+
+                  <v-list-item-subtitle
+                    >ccanersezgin@gmail.com</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+
+            <v-row class="mt-16">
+              <v-spacer></v-spacer>
+
+              <div v-for="(link, index) in cards" :key="index">
+                <v-card :to="link.to" class="mx-4" max-width="344">
+                  <v-img :src="link.img" height="344"></v-img>
+
+                  <v-card-title> {{ link.title }} </v-card-title>
+
+                  <v-card-subtitle>
+                    {{ link.subtitle }}
+                  </v-card-subtitle>
+                </v-card>
+              </div>
+              <v-spacer></v-spacer>
+            </v-row>
+          </div>
+
           <router-view></router-view>
         </v-col>
       </v-row>
@@ -47,13 +83,32 @@
 
 <script>
 export default {
+  computed: {
+    isHomepage() {
+      return this.$route.name === 'Doctor';
+    },
+    cards() {
+      return this.links.filter((link) => link.to);
+    },
+  },
   data: () => ({
-    cards: ['Today', 'Yesterday'],
     drawer: null,
     links: [
-      ['mdi-view-dashboard', 'Dashboard', ''],
-      ['mdi-map-marker-outline', 'Appointments', 'appointments'],
-      ['mdi-file-document-edit-outline', 'Prescriptions', 'prescriptions'],
+      { icon: 'mdi-view-dashboard', title: 'Dashboard', to: '', img: '' },
+      {
+        icon: 'mdi-map-marker-outline',
+        title: 'Appointments',
+        to: 'appointments',
+        img: require('@/assets/img/appointments.png'),
+        subtitle: 'Click to see the appointments assigned to you.',
+      },
+      {
+        icon: 'mdi-file-document-edit-outline',
+        title: 'Prescriptions',
+        to: 'prescriptions',
+        img: require('@/assets/img/prescriptions.jpg'),
+        subtitle: 'Click to see the prescriptions you wrote.',
+      },
     ],
   }),
 };
